bdbca1a240d4e499285ba6525d5b20d93d4cb538
Roxana Lafuente <roxannitha@gmail.com>
2016-12-30 10:35:50 -0300

    Improved code of commands file

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+"""@brief     Contains moses' commands constructors."""
+
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -22,56 +24,93 @@
 #
 ##############################################################################
 from constants import *
-import os
 
-# Process in background (non-blocking)
+
 def get_background_command(background):
+    """@brief     Runs a command in background."""
     return " &" if background else ""
 
-# Tokenizer script
+
 def get_tokenize_command(mdir, language, in_file, out_file, background=False):
+    """@brief     Constructs tokenizer command."""
     amp = get_background_command(background)
-    return "%s -l %s < %s > %s %s\n" % (tokenizer % mdir, language, in_file, out_file, amp)
+    return "%s -l %s < %s > %s %s\n" % (tokenizer % mdir,
+                                        language,
+                                        in_file,
+                                        out_file,
+                                        amp)
 
-# Truecaser train script
+
 def get_truecaser_train_command(mdir, corpus, background=False):
+    """@brief     Constructs truecaser training command."""
     amp = get_background_command(background)
-    return "%s --model %s --corpus %s %s\n" % (truecaser_train % mdir, model % mdir, corpus, amp)
+    return "%s --model %s --corpus %s %s\n" % (truecaser_train % mdir,
+                                               model % mdir,
+                                               corpus,
+                                               amp)
 
-# Truecaser script
+
 def get_truecaser_command(mdir, in_file, out_file, background=False):
+    """@brief     Constructs truecaser command."""
     amp = get_background_command(background)
-    return "%s --model %s < %s > %s %s\n" % (truecaser % mdir, model % mdir, in_file, out_file, amp)
+    return "%s --model %s < %s > %s %s\n" % (truecaser % mdir,
+                                             model % mdir,
+                                             in_file,
+                                             out_file,
+                                             amp)
 
-# Cleaner script
-def get_cleaner_command(mdir, in_lang, out_lang, in_file, out_file, background=False):
+
+def get_cleaner_command(mdir,
+                        in_lang,
+                        out_lang,
+                        in_file,
+                        out_file,
+                        background=False):
+    """@brief     Constructs cleaner command."""
     amp = get_background_command(background)
-    return "%s %s %s %s %s 1 80 %s\n" % (cleaner % mdir, in_file, out_lang, in_lang, out_file, amp)
+    return "%s %s %s %s %s 1 80 %s\n" % (cleaner % mdir,
+                                         in_file,
+                                         out_lang,
+                                         in_lang,
+                                         out_file,
+                                         amp)
 
-# LM training script
+
 def get_lmtrain_command(mdir, language, in_file, out_file, background=False):
+    """@brief     Constructs language model training command."""
     amp = get_background_command(background)
     return "%s < %s > %s %s\n" % (lm_train % mdir, in_file, out_file, amp)
 
-# Binarize LM training script
+
 def get_blmtrain_command(mdir, language, in_file, out_file, background=False):
+    """@brief     Constructs binarize language model command."""
     amp = get_background_command(background)
     return "%s %s %s %s\n" % (blm_train % mdir, in_file, out_file, amp)
 
-# Train MT system.
-def get_tmtrain_command(mdir, in_lang, out_lang, lm_file, corpus, output_dir, background=False):
-    # print "output dir:", output_dir
-    # print "current path:", os.getcwd()
+
+def get_tmtrain_command(mdir,
+                        in_lang,
+                        out_lang,
+                        lm_file,
+                        corpus,
+                        output_dir,
+                        background=False):
+    """@brief     Constructs train machine translation command."""
     amp = get_background_command(background)
     cmd = tm_train % mdir
 
     cmd += " -cores 6 -corpus " + corpus + " -f " + in_lang + " -e " + out_lang
-    cmd += " -alignment grow-diag-final-and -reordering msd-bidirec"
-    cmd += "tional-fe -lm 0:3:" + lm_file.replace("~", "$HOME")
-    cmd += " -external-bin-dir " + mdir + "/scripts >& " + output_dir + "/" + train_fn
+    cmd += " -alignment grow-diag-final-and -reordering msd-bidirectional-fe "
+    cmd += "-lm 0:3:" + lm_file.replace("~", "$HOME") + " -external-bin-dir "
+    cmd += mdir + "/scripts >& " + output_dir + "/" + train_fn
     return cmd + amp + "\n"
 
-# Run the decoder.
+
 def get_test_command(mdir, moses_ini, in_file, out_file, background=False):
+    """@brief     Constructs run decoder command."""
     amp = get_background_command(background)
-    return "%s %s < %s > %s %s\n" % (test % mdir, moses_ini, in_file, out_file, amp)
+    return "%s %s < %s > %s %s\n" % (test % mdir,
+                                     moses_ini,
+                                     in_file,
+                                     out_file,
+                                     amp)
