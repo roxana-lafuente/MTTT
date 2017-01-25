8d6c1d37e943f48223391f5981c6488f704194b8
miguelemosreverte <miguellemos95@gmail.com>
2017-01-25 02:01:30 -0300

    Solved the problem where GTM would just now send the text to subprocess.PIPE. I had to add stderr=subprocess.PIPE. Now it works. That and added more meaningful answers in the cases where errors where caused by the evaluation scripts.

--- evaluation.py
+++ evaluation.py
@@ -42,35 +42,44 @@ def creation_date(path_to_file):
             # so we'll settle for when its content was last modified.
             return stat.st_mtime
 
-def filterTER (line):
-    if "Total TER:" in line:
-        line = line.replace("Total TER:", "")
-        line = line.split('(', 1)[0]
-        line += "\n"
-        return line
-    else:
-        return ''
+def filterTER (lines):
+    result = ''
+    warning = False
+    lines = lines.splitlines()
+    for line in lines:
+        print line
+        if "Total TER:" in line:
+            result += line
+        if "Warning" in line:
+            warning = True
+    if warning:
+        result += " There are lines unchanged from source to reference. HTER cannot work in those cases."
+    return result + "\n"
 
 def filterBLEU (line, BLEU_type):
     if BLEU_type == "BLEU":      line = line.split(',', 1)[0]
     if BLEU_type == "BLEU2GRAM": line = line.split(',', 1)[1].split('/')[0]
     if BLEU_type == "BLEU3GRAM": line = line.split(',', 1)[1].split('/')[1]
     if BLEU_type == "BLEU4GRAM": line = line.split(',', 1)[1].split('/')[2]
+    line = line.replace('\n','').replace('\r', '')
     return line
 
-def filter_verbose_output(output,method):
-    specific_line = ""
-    while True:
-      line = output.stdout.readline()
-      if line != '':
-        if method == "TER": filtered_line = filterTER(line)
-        #add more complicated methods to filter over here
+def filterGTM (line):
+    print line
+    if "You should not be comparing equal runs" in line:
+        line = "There are lines unchanged from source to reference. GTM cannot work in those cases.\n"
+    return line
 
-        if filtered_line != '':
-            specific_line = filtered_line
-      else:
-        break
-    return specific_line
+def filter_output(proccess,method):
+    out, err = proccess.communicate()
+    final_text = ""
+    if not err :
+        final_text = out
+    else: final_text = err
+    if method == "TER": final_text = filterTER(final_text)
+    if method == "GTM": final_text = filterGTM(final_text)
+
+    return final_text
 
 cached_results = {}
 
@@ -98,22 +107,25 @@ def evaluate(checkbox_indexes, test, reference):
 
                 if checkbox_indexes_constants[checkbox_index] == "HTER":
                     command_2 = EXEC_JAVA + "-cp " + TER_DIRECTORY + " TERtest " + " -r " + reference + " -h " + test
-                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE)
-                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_verbose_output(proc,"TER")
+                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_output(proc,"TER")
                     return_results += result
                     cached_results[key] =  result
 
 
                 if checkbox_indexes_constants[checkbox_index] == "GTM":
                     command_2 = EXEC_JAVA + "-cp " + GTM_DIRECTORY + " gtm" + " -t " +  test + " " + reference
-                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE)
-                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_verbose_output(proc,"GTM")
+                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_output(proc,"GTM")
+
+                    #out, err = proc.communicate()
+                    #result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + out + err
                     return_results += result
                     cached_results[key] =  result
 
                 if "BLEU" in checkbox_indexes_constants[checkbox_index] and BLEU_cached_results == "":
                     command = EXEC_PERL + DIRECTORY + "BLEU.pl " + test +" < " + reference
-                    proc = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE)
+                    proc = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     while True:
                       line = proc.stdout.readline()
                       if line != '':BLEU_cached_results += line
